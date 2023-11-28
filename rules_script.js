1bef616924613371322104c74f5ab387dc6099af
ksenialipaeva <57674966+ksenialipaeva@users.noreply.github.com>
2023-11-28 16:10:30 +0300

    Add files via upload

---
+++ rules_script.js
@@ -0,0 +1,37 @@
+var i = 0;
+var count = 3;
+
+function next(){
+    var id = "text"+i;
+    var t = document.getElementById(id);
+    t.style.display = "none";
+    i++;
+    if (i >= count){
+        i=count-1;
+    }
+    if (i==count-1){
+        var b = document.getElementById("next");
+        b.style.opacity = 0;
+    }
+    var id = "text"+i;
+    var t = document.getElementById(id);
+    t.style.display = "inline";
+}
+
+function prev(){
+    var id = "text"+i;
+    var t = document.getElementById(id);
+    t.style.display = "none"
+    i--;
+    if (i < 0){
+        i=0;
+    }
+    if (i==0){
+        var b = document.getElementById("prev");
+        b.style.opacity = 0;
+    }
+    var id = "text"+i;
+    var t = document.getElementById(id);
+    t.style.display = "inline";
+        
+}
